9d2f082e89cc6f13179d6ddfae3ec63ae23a556c
Vadym_Usyk <vadym_usyk@epam.com>
2022-09-18 19:16:35 +0300

    change

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ exports.handler = (event, context, callback) => {
     emitLambdaAge();
 
     // This variable can be updated and checked in to your repository 
-    // to update the number of SAM squirrels on the screen.
     var samCount = 10;
     // Or you can update your Lambda function's environment variable.
     var samMultiplier = process.env.SAM_MULTIPLIER || 1;
